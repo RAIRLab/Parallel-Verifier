e6519eb6316f5fa8ea9d652808105424a09e7326
Brandon Rozek <brozek@brandonrozek.com>
2023-01-25 10:20:33 -0500

    Refacted serial verifier code

--- src/SerialVerifier.cpp
+++ src/SerialVerifier.cpp
@@ -7,53 +7,50 @@
 #include"SharedVerifier.hpp"
 #include"timing.hpp"
 
+std::tuple<std::unordered_map<vertId, size_t>, size_t> computerLayerMap(const Proof& p) {
+    // VertexId -> Layer Number
+    std::unordered_map<vertId, size_t> layerMap;
+
+    // Iterate through all the nodes starting
+    // from the assumptions down to the leaves
+    std::queue<std::tuple<vertId, size_t>> nodes;
+
+    // Assumptions are at layer 0
+    for (vertId vertexId : p.assumptions) {
+        nodes.push(std::make_tuple(vertexId, 0));
+    }
+
+    size_t maxLayerNum = 0;
+    while (!nodes.empty()) {
+        // Grab next vertexId
+        const auto [vertexId, layer] = nodes.front();
+        nodes.pop();
+
+        // Replace any lower value layer number with the current layer
+        // number
+        layerMap[vertexId] = layer;
+
+        // Keep track of the max layer number
+        maxLayerNum = std::max(maxLayerNum, layer);
+
+        // Trascend downward: Add children to nodes
+        const auto n = p.nodeLookup.at(vertexId);
+        for (const vertId &childId : n.children) {
+            nodes.push(std::make_tuple(childId, layer + 1));
+        }
+    }
+
+    return std::make_tuple(layerMap, maxLayerNum);
+}
+
 std::vector<std::vector<vertId>> computeLayers(const Proof& p) {
-    const auto [layerNum, maxLayerNum] = [&p]() {
-        // VertexId -> Layer Number
-        std::unordered_map<vertId, int> layerNum;
-
-        // Iterate through all the nodes starting
-        // from the assumptions down to the leaves
-        std::queue<std::tuple<vertId, int>> nodes;
-
-        // Assumptions are at layer 0
-        for (vertId vertexId : p.assumptions) {
-            nodes.push(std::make_tuple(vertexId, 0));
-        }
-
-        int maxLayerNum = 0;
-        while (!nodes.empty()) {
-            // Grab next vertexId
-            const auto [vertexId, layer] = nodes.front();
-            nodes.pop();
-
-            // Replace any lower value layer number with the current layer
-            // number
-            layerNum[vertexId] = layer;
-
-            // Keep track of the max layer number
-            if (layer > maxLayerNum) {
-                maxLayerNum = layer;
-            }
-
-            // Trascend downward: Add children to nodes
-            const auto n = p.nodeLookup.at(vertexId);
-            for (const vertId &childId : n.children) {
-                nodes.push(std::make_tuple(childId, layer + 1));
-            }
-        }
-        return std::make_tuple(layerNum, maxLayerNum);
-    }();
-
+    const auto [layerNum, maxLayerNum] = computerLayerMap(p);
 
     // Construct layers from layerNum map
-    const auto layers = [&maxLayerNum, &layerNum]() {
-        std::vector<std::vector<vertId>> result(maxLayerNum + 1, std::vector<vertId>());
-        for (const auto & [ vertexId, layer ] : layerNum) {
-            result.at(layer).push_back(vertexId);
-        }
-        return result;
-    }();
+    std::vector<std::vector<vertId>> layers(maxLayerNum + 1, std::vector<vertId>());
+    for (const auto & [ vertexId, layer ] : layerNum) {
+        layers.at(layer).push_back(vertexId);
+    }
 
     return layers;
 }
