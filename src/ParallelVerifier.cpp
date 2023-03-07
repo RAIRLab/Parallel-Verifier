b9502ee947bee3f95a2ca1958b5ea86758b3b4ab
James Oswald <jamesoswald111@gmail.com>
2023-03-07 01:18:42 -0500

    Layer map computation

--- src/ParallelVerifier.cpp
+++ src/ParallelVerifier.cpp
@@ -14,13 +14,12 @@
 int myRank;
 int numRanks;
 
+//Depthmaps map each node in the proof graph to a layer number
+//representing the maximum distance to an assumption
 using DepthMap = std::unordered_map<VertId, size_t>;
 
-//Pseudo-Marking struct
-struct Marking{
-    VertId node;
-    bool completed;
-};
+//Layer Map takes a layer and returns a list of nodes on that layer.  
+using LayerMap = std::unordered_map<size_t, std::unordered_set<VertId>>;
 
 void MPIDebugPrint(std::string message, int rank = -1){
     MPI_Barrier(MPI_COMM_WORLD);
@@ -67,26 +66,45 @@ std::vector<int> getRankDisplacements(const std::vector<int>& rankSizes){
 }
 
 
-//Takes a proof and returns a map from vertices to 
-//their maximum distance to an assumption 
-//Climbs down the proof tree from the assumptions
-DepthMap getDepthMap(const Proof& p){
-    DepthMap depthMap;
-    std::unordered_set<VertId> potentialLayerMembers;
+//Takes a proof and returns a map from layer numbers to sets of vertices
+//on that layer.
+//Works by climbing down the proof tree from the assumptions 
+//Because it starts at assumptions, the map is not gaurnteed to contain
+//every node in P if the node is stand alone without any assumptions as
+//a parent, however these cases only occour is a proof is false and
+//will not be verified
+std::pair<LayerMap, DepthMap> getLayerAndDepthMaps(const Proof& p){
     
+    //Maps each node (VertID) to its layer number (size_t)
+    DepthMap depthMap; 
+    //Maps each layer number (size_t) to the set of nodes
+    //(std::unordered_set<VertId>) on that layer
+    LayerMap layerMap; 
+
+    //Stores the nodes that are posibly members of the current layer
+    std::unordered_set<VertId> potentialLayerMembers;
+
     //Add all assumptions to the depth map at level 0
-    // and add children as potential level 1
+    //and their children as possible members of layer 1
+    layerMap.insert({0, std::unordered_set<VertId>()});
     for(const VertId assumption : p.assumptions){
         depthMap.insert({assumption, 0});
+        layerMap[0].insert(assumption);
         for(VertId assumptionChild : p.nodeLookup.at(assumption).children){
             potentialLayerMembers.insert(assumptionChild);
         }
     }
 
-    using PotentialMemberIterator = std::unordered_set<VertId>::const_iterator;
+    //While there are nodes that could potenially be members of the current layer
+    //(stored within potentialLayerMembers) split them up among each rank
+    //and determine if they actually are a member of the current layer
+    //by if they are fully marked (all of their parents are in the depthmap)
+    //if so they can be added to the current layer and their children made
+    //contenders for the next layer. 
     size_t currentLayer = 1;
     while(potentialLayerMembers.size() != 0){
         
+        //Figure out what our rank is handling on this iteration
         size_t numItems = potentialLayerMembers.size();
         std::vector<int> rankSizes = getRankSizes(numItems);
         std::vector<int> rankDisplacements = getRankDisplacements(rankSizes);
@@ -94,30 +112,32 @@ DepthMap getDepthMap(const Proof& p){
         int myDisplacement = rankDisplacements[myRank];
         
         //Buffers for storing the MPI communications 
+        //uint_8t used as a standin for bool so it plays nice
+        //with MPI and I don't need to deal with vector<bool> drama
         std::vector<int> vertIdSendBuf(myItemCount);
         std::vector<int> vertIdReceiveBuf(numItems);
         std::vector<uint8_t> inLayerSendBuf(myItemCount);
         std::vector<uint8_t> inLayerReceiveBuf(numItems);
 
-        //MPIDebugPrint("Layer: " + std::to_string(currentLayer));
-
+        using PotentialMemberIterator = \
+              std::unordered_set<VertId>::const_iterator;
         PotentialMemberIterator iter = potentialLayerMembers.begin();
         PotentialMemberIterator endIter = potentialLayerMembers.end();
 
-        //MPIDebugPrint("Before Advance:");
-
         std::advance(iter, myDisplacement);
 
-        //MPIDebugPrint("Offset: " + std::to_string(myDisplacement));
-
-        //Compute if a node is on a layer in parallel
+        //Compute if a node is on a layer by checking if all its
+        //parents are in the depth map
         for(size_t i = 0; i < myItemCount && iter != endIter; i++, iter++){
             vertIdSendBuf[i] = *iter;
             inLayerSendBuf[i] = 
                 (uint8_t)allParentsInDepthMap(p, *iter, depthMap);
         }
 
-        //Share the information about who's on the current layer
+        //Share which of our nodes are on the current layer
+
+        //TODO: This first MPI call is probably useless since you can
+        //compute localy what nodes are where in potentialLayerMembers.
         MPI_Allgatherv((void*)vertIdSendBuf.data(), myItemCount,\
                       MPI_INT, (void*)vertIdReceiveBuf.data(), \
                       rankSizes.data(), rankDisplacements.data(),\
@@ -127,10 +147,14 @@ DepthMap getDepthMap(const Proof& p){
                       rankSizes.data(), rankDisplacements.data(),\
                       MPI_UINT8_T, MPI_COMM_WORLD);
 
+        //Update our maps and mark the children of completed nodes
+        //as being possible nodes on the next layer
+        layerMap.insert({currentLayer, std::unordered_set<VertId>()});
         for(int i = 0; i < numItems; i++){
             VertId completed = vertIdReceiveBuf[i];
             if(inLayerReceiveBuf[i]){
                 depthMap.insert({completed, currentLayer});
+                layerMap[currentLayer].insert(completed);
                 potentialLayerMembers.erase(completed);
                 for(VertId compChild : p.nodeLookup.at(completed).children){
                     potentialLayerMembers.insert(compChild);
@@ -140,30 +164,36 @@ DepthMap getDepthMap(const Proof& p){
         currentLayer++;
     }
 
-    if(myRank == 0){
-        for(const auto[id, depth] : depthMap){
-            std::cout<<id<<" "<<depth<<std::endl;
-        }
-    }
-
-    return depthMap;
+    return std::make_pair(layerMap, depthMap);
 }
 
 bool verifyParallel(const Proof& p){
-    DepthMap depthMap = getDepthMap(p);
+    auto [layerMap, depthMap] = getLayerAndDepthMaps(p);
     if(depthMap.size() != p.nodeLookup.size())
         return false;
 
+    if(myRank == 0){
+        for(const auto [layer, set] : layerMap){
+            std::cout<<"Layer "<<layer<<":";
+            for(const auto id : set){
+                std::cout<<id<<",";
+            }
+            std::cout<<"\b"<<std::endl;
+        }
+    }
+    
     return true;
 }
 
 std::string getFileContentsMPI(const char* filePath){
     MPI_File fileHandle;
-    MPI_File_open(MPI_COMM_WORLD, filePath, MPI_MODE_RDONLY, MPI_INFO_NULL, &fileHandle);
+    MPI_File_open(MPI_COMM_WORLD, filePath, MPI_MODE_RDONLY,
+                  MPI_INFO_NULL, &fileHandle);
     MPI_Offset fileSize;
     MPI_File_get_size(fileHandle, &fileSize);
     char* fileContentsBuff = new char[fileSize];
-    MPI_File_read(fileHandle, fileContentsBuff, fileSize, MPI_CHAR, MPI_STATUS_IGNORE);
+    MPI_File_read(fileHandle, fileContentsBuff, fileSize,
+                  MPI_CHAR, MPI_STATUS_IGNORE);
     MPI_File_close(&fileHandle);
     std::string fileContents = std::string(fileContentsBuff);
     delete[] fileContentsBuff;
