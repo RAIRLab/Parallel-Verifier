d12f244c2c6980d21db807d59bf56c35a3a5c75b
James Oswald <jamesoswald111@gmail.com>
2023-04-24 18:22:12 -0400

    New version of BF

--- src/LibOMPVerifier/verifyOpenMPBF.cpp
+++ src/LibOMPVerifier/verifyOpenMPBF.cpp
@@ -2,15 +2,18 @@
 #include "../SharedVerifier/SharedVerifier.hpp"
 #include "LibOMPVerifier.hpp"
 
+using FastLayerMap = std::vector<std::vector<VertId>>;
+using FastDepthMap = std::vector<size_t>;
+using NodeVec = std::vector<VertId>;
 
 //Recursive helper for getLayerAndDepthMapsSerial
 //Returns the depth of a node id, in a proof p, using and modifying
 //a global depth map for the proof.
-size_t getNodeDepthBF(const Proof& p, VertId id, DepthMap& depthMap){
+size_t getNodeDepthBF(const Proof& p, VertId id, FastDepthMap& depthMap){
     //Base case 1, we already know the depth of the node
-    DepthMap::const_iterator depthMapIter = depthMap.find(id);
-    if(depthMapIter != depthMap.end()){
-        return depthMapIter->second;
+    size_t depthMapVal = depthMap[id];
+    if(depthMapVal != -1){
+        return depthMapVal;
     }
     //Base case 2, we're an assumption
     std::unordered_set<VertId> parents = p.nodeLookup.at(id).parents;
@@ -32,14 +35,11 @@ size_t getNodeDepthBF(const Proof& p, VertId id, DepthMap& depthMap){
     depthMap[id] = maxDepth + 1;
     return maxDepth + 1;
 }
-
-using FastLayerMap = std::vector<std::vector<VertId>>;
-using NodeVec = std::vector<VertId>;
 //O(n) serial construction of the depth and layer maps for a proof.
 //n is the number of nodes in the proof
 std::pair<FastLayerMap, NodeVec> getLayerMapBF(const Proof& p){
     //Depthmap construction is O(n) via dynamic programming
-    DepthMap depthMap;
+    FastDepthMap depthMap(p.nodeLookup.size(), -1);
     NodeVec allNodes;
     allNodes.reserve(p.nodeLookup.size());
     size_t maxDepth = 0;
@@ -54,12 +54,20 @@ std::pair<FastLayerMap, NodeVec> getLayerMapBF(const Proof& p){
     //LayerMap construction O(n)
     //std::list fill constructor
     FastLayerMap layerVector(maxDepth + 1, std::vector<VertId>());
-    for(const auto& [id, depth] : depthMap){
-        layerVector[depth].push_back(id);
+    for(int i = 0; i < depthMap.size(); i++){
+        layerVector[depthMap[i]].push_back(i);
     }
 
     return std::make_pair(layerVector, allNodes);
 }
+
+/*
+std::pair<FastLayerMap, NodeVec> getLayerMapBFParallel(const Proof& p){
+    FastDepthMap depthMap(p.nodeLookup.size(), -1);
+    NodeVec allNodes;
+    allNodes.reserve(p.nodeLookup.size());
+    assumptions = p.assumptions;
+}*/
 
 bool OMPVerifier::OMPVerifyBF(const Proof& p){
 
